7a28afe93585446a57450e89b5c8ec013319f2ef
shino <shinobufan@free.fr>
2022-11-20 19:35:33 +0100

    Fix deprecated PIL resampling

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def preprocess(image, sizemax):
     w, h = sizemax
     w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
     image = image.resize((w, h))
-    image.thumbnail(sizemax, resample=PIL.Image.LANCZOS)
+    image.thumbnail(sizemax, resample=PIL.Image.Resampling.LANCZOS)
     image = np.array(image).astype(np.float32) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     image = torch.from_numpy(image)
